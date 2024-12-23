ccdeebb28e56d9263d9b1323112e963950cb95fa
Tirth chokshi <chokshitirth4@gmail.com>
2024-12-23 11:04:27 +0530

    total customer count get api done

--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -1,9 +1,11 @@
 import express from "express"
-import { getAllCustomers,getCustomerById,deleteCustomer,updateCustomer,createCustomer,bulkUploadCustomers } from "../controllers/customerControllers.js"
+import { getTotalCustomers,getAllCustomers,getCustomerById,deleteCustomer,updateCustomer,createCustomer,bulkUploadCustomers } from "../controllers/customerControllers.js"
 
 const router = express.Router()
 
 router.get('/',getAllCustomers)
+ // you have to put specific route first to make them work /total comes before then /:id
+router.get('/total',getTotalCustomers)
 router.get('/:id',getCustomerById)
 router.delete('/:id',deleteCustomer)
 router.put('/:id',updateCustomer)
