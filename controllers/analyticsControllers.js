aaa3122179a9d6c96dbf289c3fb8d7f8efd0968b
Tirth chokshi <chokshitirth4@gmail.com>
2024-12-27 15:26:12 +0530

    charts api done

--- controllers/analyticsControllers.js
+++ controllers/analyticsControllers.js
@@ -1,7 +1,7 @@
-import db from "../config/db.js"
+import db from "../config/db.js";
 
 export const getTopCustomers = (req, res) => {
-    const query = `
+  const query = `
     SELECT 
         ca.customer_id, 
         c.name AS customer_name, 
@@ -15,14 +15,14 @@ export const getTopCustomers = (req, res) => {
     ORDER BY 
         total_activities DESC;
     `;
-    db.query(query, (error, results) => {
-        if (error) return res.status(500).json({ error: error.message });
-        res.status(200).json(results);
-    });
-}
+  db.query(query, (error, results) => {
+    if (error) return res.status(500).json({ error: error.message });
+    res.status(200).json(results);
+  });
+};
 
 export const actiivityTypeDistribution = (req, res) => {
-    const query = `
+  const query = `
     SELECT 
         ca.activity_type, 
         COUNT(ca.customer_activity_id) AS total_activities
@@ -31,8 +31,30 @@ export const actiivityTypeDistribution = (req, res) => {
     GROUP BY 
         ca.activity_type;
     `;
-    db.query(query, (error, results) => {
-        if (error) return res.status(500).json({ error: error.message });
-        res.status(200).json(results);
-    });
-}
+  db.query(query, (error, results) => {
+    if (error) return res.status(500).json({ error: error.message });
+    res.status(200).json(results);
+  });
+};
+
+export const caseResolution = (req, res) => {
+  const query = `
+   SELECT 
+    DATE(next_followup_date) as date,
+    SUM(CASE WHEN case_resolved = 'Resolved' THEN 1 ELSE 0 END) as resolved_cases,
+    SUM(CASE WHEN case_resolved IN ('Not Resolved', 'Pending') THEN 1 ELSE 0 END) as remaining_cases
+FROM 
+    customer_activity
+WHERE 
+    next_followup_date >= DATE_SUB(CURDATE(), INTERVAL 7 DAY)
+    AND next_followup_date <= CURDATE()
+GROUP BY 
+    DATE(next_followup_date)
+ORDER BY 
+    date;
+    `;
+  db.query(query, (error, results) => {
+    if (error) return res.status(500).json({ error: error.message });
+    res.status(200).json(results);
+  });
+};
--- routes/analyticsRoutes.js
+++ routes/analyticsRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getTopCustomers, actiivityTypeDistribution } from "../controllers/analyticsControllers.js";
+import { getTopCustomers, actiivityTypeDistribution, caseResolution } from "../controllers/analyticsControllers.js";
 
 const router = express.Router();
 
 router.get("/topcustomers", getTopCustomers);
 router.get("/activitytype", actiivityTypeDistribution);
+router.get("/caseresolution", caseResolution);
 
 export default router;
